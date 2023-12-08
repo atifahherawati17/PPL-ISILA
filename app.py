977b94fa2ebf6bc85c1c4dab155eaa585e014a32
Tiara Humaira Putri <20102196@ittelkom-pwt.ac.id>
2023-12-08 23:10:10 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('indexweb.html')
+
+@app.route("/loginadm")
+def loginadm():
+    return render_template('loginadmin.html')
+
+@app.route("/loginpmh")
+def loginpmh():
+    return render_template('loginpemohon.html')
+
+if __name__ == '__main__':
+    app.run (debug = True)
